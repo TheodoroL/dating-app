319901c43582534bd2547ec407cbf532b6ed52e3
Lucas <lucastheodoromourofernandes@gmail.com>
2025-10-27 16:47:52 -0400

    feat: adicionar componentes de botão e input com estilos e funcionalidades

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from "./components/button/button.component";
+import { InputComponent } from "./components/input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
