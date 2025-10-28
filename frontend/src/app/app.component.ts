1b8cdb88014dbe10729be4074fde8681318fc4ae
Lucas <lucastheodoromourofernandes@gmail.com>
2025-10-28 13:27:23 -0400

    feat: remover componentes Button e Input do app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from "./components/button/button.component";
-import { InputComponent } from "./components/input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, InputComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
 }
